8b76ebae1cbb19925deb7247d45df8b3f6b5b337
magic-doggo <109861946+magic-doggo@users.noreply.github.com>
2025-10-24 09:24:41 +0000

    Initial commit

---
+++ db/pool.js
@@ -0,0 +1,13 @@
+const { Pool } = require("pg");
+
+module.exports = new Pool({
+  connectionString: "postgresql://<role_name>:<role_password>@localhost:5432/top_users"
+});
+
+// module.exports = new Pool({
+//   connectionString: process.env.EXTERNAL_DB_URL,
+//   ssl: {
+//     require: true,
+//     rejectUnauthorized: false
+//   }
+// });
